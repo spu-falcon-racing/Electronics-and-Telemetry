5cea8c21d66b1d4b011411bbdda7b86190f43c34
Quinton Cline <clineq@spu.edu>
2019-02-18 16:31:27 -0800

    Shows analog values to serialPort

--- sketch_1.0/sketch_1.0.ino
+++ sketch_1.0/sketch_1.0.ino
@@ -2,15 +2,18 @@ int lBkTire = 0; //Reading from linear potentiometer from left back tire
 int lFtTire = 0; // " " " " " left front tire
 int rBkTire = 0; // " " " " " right back tire
 int rFtTire = 0; // " " " " " right front tire
-
+int input=A0;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lBkTire = analogRead(1);
-  lFtTire = analogRead(2);
-  rBkTire = analogRead(3);
-  rFtTire = analogRead(4);
+  lBkTire = analogRead(input);
+  //lFtTire = analogRead(2);
+  //rBkTire = analogRead(3);
+  //rFtTire = analogRead(4);
+  Serial.println(lBkTire); 
+  delay(100);
 }
